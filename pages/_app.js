d409594488a3d07c91a8cebe7e24e751d90e8f7e
lukmanx <archer.lukman@gmail.com>
2021-11-22 22:32:46 +0700

    update temporary

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { Provider } from "react-redux";
+import store from "../redux";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+
+
+  )
 }
 
 export default MyApp
