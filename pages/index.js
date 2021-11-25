64f2a239db31235f554026fb72ad73df2f406219
lukmanx <archer.lukman@gmail.com>
2021-11-25 22:19:18 +0700

    test

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css';
+import Header from "../components/header"
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <h1 className={styles["title-homepage"]}> Welcome Lukman</h1>
+    </>
+
+  )
+
+}
