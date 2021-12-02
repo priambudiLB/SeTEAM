633a007d6118c4b0d9fe9552ce55941c940094af
lukmanx <archer.lukman@gmail.com>
2021-12-02 23:02:58 +0700

    - create main Layout - set Navbar home as Navbar utama - set semua halaman sebagai child dari NavbarHome - hapus component NavbarHome di home.js

---
+++ components/layout/layout.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import NavbarHome from "../navbarhome";
+
+
+const Layout = (props) => {
+
+    return (
+        <Fragment>
+            <NavbarHome />
+            <main> {props.children}</main>
+
+        </Fragment>
+    )
+
+}
+
+export default Layout;
