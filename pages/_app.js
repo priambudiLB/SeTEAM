826f217cbfbba8a38e3a440429808241accd3bc1
lukmanx <archer.lukman@gmail.com>
2021-12-06 22:47:32 +0700

    merge local dev + branch_lkm

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
-<<<<<<< HEAD
 
 
 
 import { Provider } from "react-redux";
 import store from "../redux";
-=======
->>>>>>> 6d75f4b4d7c0f3be9dfa502f6ff7df5cf57b2d7a
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/layout";
 
@@ -23,10 +20,6 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-<<<<<<< HEAD
 
 export default MyApp
 
-=======
-export default MyApp;
->>>>>>> 6d75f4b4d7c0f3be9dfa502f6ff7df5cf57b2d7a
