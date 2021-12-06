50918356df824a3ea27e41ecd771dfcc6a4fbb5b
lukmanx <archer.lukman@gmail.com>
2021-12-06 23:37:47 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 
 export default (req, res) => {
     res.status(200).json({ name: 'John Doe' })
   }
   
-=======
-export default function handler (req, res){
-    res.status(200).json({ name: 'John Doe' })
-}
->>>>>>> 6d75f4b4d7c0f3be9dfa502f6ff7df5cf57b2d7a
