d049b01d6ee6033f46ca76d7cd7cd2ced32d2ea2
lukmanx <archer.lukman@gmail.com>
2021-12-12 16:02:18 +0700

    mapping data property in Instructor Courses

--- pages/InstructorCourses.js
+++ pages/InstructorCourses.js
@@ -1,5 +1,5 @@
 import styles from '../styles/homepage.module.css'
-import { Wrap, WrapItem, Text, Center, Box, Heading, Container, Stack } from "@chakra-ui/layout"
+import { Wrap, WrapItem, Text, Center, Flex, Box, Heading, Container, Stack } from "@chakra-ui/layout"
 import { Image } from "@chakra-ui/image"
 import { Badge } from "@chakra-ui/layout"
 import { StarIcon } from "@chakra-ui/icons"
@@ -9,6 +9,7 @@ import { VidCoursesContextProvider } from "../context/AddVideoContext"
 export default function Home() {
   const property = [
     {
+      id: 0,
       imageUrl: '/stats.png',
       imageAlt: 'Rear view of modern home with pool',
       author1: 'Daniel Costa',
@@ -19,6 +20,7 @@ export default function Home() {
       rating: 4,
     },
     {
+      id: 1,
       imageUrl: '/stats.png',
       imageAlt: 'Rear view of modern home with pool',
       author1: 'Beni Moore',
@@ -36,112 +38,60 @@ export default function Home() {
       {property.map((data) => {
 
         return (
-          <Container display='contents' >
-            <Center>
-              <Wrap >
-                <WrapItem flexDir={"col"}>
-                  <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    <Image src={data.imageUrl} alt={data.imageAlt} />
-                    <Box p='6'>
-                      <Box display='flex' alignItems='baseline'>
-                        <Badge borderRadius='full' px='2' colorScheme='teal'>
-                          New
-                        </Badge>
-                        <Box
-                          color='gray.500'
-                          fontWeight='semibold'
-                          letterSpacing='wide'
-                          fontSize='xs'
-                          textTransform='uppercase'
-                          ml='2'
-                        >
-                          Instructor : {data.author1}  &bull; {data.author2}
-                        </Box>
-                      </Box>
-                      <Box
-                        mt='1'
-                        fontWeight='semibold'
-                        as='h4'
-                        lineHeight='tight'
-                        isTruncated
-                      >
-                        {data.title}
-                      </Box>
-                      <Box>
-                        {data.formattedPrice}
-                        <Box as='span' color='gray.600' fontSize='sm'>
-                          / wk
-                        </Box>
-                      </Box>
 
-                      <Box display='flex' mt='2' alignItems='center'>
-                        {Array(5)
-                          .fill('')
-                          .map((_, i) => (
-                            <StarIcon
-                              key={i}
-                              color={i < data.rating ? 'teal.500' : 'gray.300'}
-                            />
-                          ))}
-                        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-                          {data.reviewCount} reviews
-                        </Box>
-                      </Box>
+            <Center key={data.id}>
+              <Box maxW='sm' borderWidth='1px' borderRadius='lg' >
+                <Image src={data.imageUrl} alt={data.imageAlt} />
+                <Box p='6'>
+                  <Box display='flex' >
+                    <Badge borderRadius='full' px='2' colorScheme='teal'>
+                      New
+                    </Badge>
+                    <Box
+                      color='gray.500'
+                      fontWeight='semibold'
+                      letterSpacing='wide'
+                      fontSize='xs'
+                      textTransform='uppercase'
+                      ml='2'
+                    >
+                      Instructor : {data.author1}  &bull; {data.author2}
                     </Box>
                   </Box>
-                  <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    <Image src={data.imageUrl} alt={data.imageAlt} />
-                    <Box p='6'>
-                      <Box display='flex' alignItems='baseline'>
-                        <Badge borderRadius='full' px='2' colorScheme='teal'>
-                          New
-                        </Badge>
-                        <Box
-                          color='gray.500'
-                          fontWeight='semibold'
-                          letterSpacing='wide'
-                          fontSize='xs'
-                          textTransform='uppercase'
-                          ml='2'
-                        >
-                          Instructor : {data.author1}  &bull; {data.author2}
-                        </Box>
-                      </Box>
-                      <Box
-                        mt='1'
-                        fontWeight='semibold'
-                        as='h4'
-                        lineHeight='tight'
-                        isTruncated
-                      >
-                        {data.title}
-                      </Box>
-                      <Box>
-                        {data.formattedPrice}
-                        <Box as='span' color='gray.600' fontSize='sm'>
-                          / wk
-                        </Box>
-                      </Box>
-
-                      <Box display='flex' mt='2' alignItems='center'>
-                        {Array(5)
-                          .fill('')
-                          .map((_, i) => (
-                            <StarIcon
-                              key={i}
-                              color={i < data.rating ? 'teal.500' : 'gray.300'}
-                            />
-                          ))}
-                        <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-                          {data.reviewCount} reviews
-                        </Box>
-                      </Box>
+                  <Box
+                    mt='1'
+                    fontWeight='semibold'
+                    as='h4'
+                    lineHeight='tight'
+                    isTruncated
+                  >
+                    {data.title}
+                  </Box>
+                  <Box>
+                    {data.formattedPrice}
+                    <Box as='span' color='gray.600' fontSize='sm'>
+                      / wk
                     </Box>
                   </Box>
-                </WrapItem>
-              </Wrap>
-            </Center>
-          </Container>
+                  <Box display='flex' mt='2'>
+                    {Array(5)
+                      .fill('')
+                      .map((_, i) => (
+                        <StarIcon
+                          key={i}
+                          color={i < data.rating ? 'teal.500' : 'gray.300'}
+                        />
+                      ))}
+                    <Box as='span' ml='2' color='gray.600' fontSize='sm'>
+                      {data.reviewCount} reviews
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+              </Center>
+
+
+
         )
       })}
 
